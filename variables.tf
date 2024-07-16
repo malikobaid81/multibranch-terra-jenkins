b4fb3f73a36d80101d5432cd8940a531990be092
malikobaid81 <122255795+malikobaid81@users.noreply.github.com>
2024-07-16 15:31:10 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,16 +3,6 @@ variable "aws_region" {
        default     = "ap-south-1" 
 }
 
-variable "vpc_name" {
-       description = "The AWS vpc to create." 
-       default     = "terra-jenkins-vpc" 
-}
-
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "terra-jenkins-SG" 
-} 
-
 variable "instance_type" { 
     description = "instance type for ec2" 
     default     =  "t2.micro" 
@@ -23,7 +13,7 @@ variable "ami_id" {
     default     = "ami-0ad21ae1d0696ad58" 
 }
 
-variable "tag_name" { 
+variable "ec2_name" { 
     description = "Tag Name of for Ec2 instance" 
     default     = "terra-jenkins-ec2" 
 }
