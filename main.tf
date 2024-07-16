94182621d62ad412075bddfb797baaf98739cec2
malikobaid81 <122255795+malikobaid81@users.noreply.github.com>
2024-07-16 15:24:01 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,47 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
-  instance_tenancy = "default"
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
   tags = {
-    Name = var.vpc_name
+      Name = "TF-Instance"
   }
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "terra-jenkins-sg" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# Inbound rules
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound rule
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
+/*
 # create ec2 instance
 resource "aws_instance" "terra-jenkins-instance" {
   ami           = var.ami_id
@@ -52,3 +20,4 @@ resource "aws_instance" "terra-jenkins-instance" {
     Name = var.tag_name
   }
 }
+*/
