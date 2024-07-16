59335f074d57e6abdbf3c935eafcf0d80bc375f0
malikobaid81 <122255795+malikobaid81@users.noreply.github.com>
2024-07-16 16:45:32 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,53 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
-  instance_tenancy = "default"
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region
+}
+
+resource "aws_vpc" "multi_vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
     Name = var.vpc_name
-  }
-}
-
-#Create security group with firewall rules
-resource "aws_security_group" "terra-jenkins-sg" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# Inbound rules
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound rule
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
-# create ec2 instance
-resource "aws_instance" "terra-jenkins-instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.terra-jenkins-sg.id]
-  tags= {
-    Name = var.tag_name
   }
 }
