f205cff3d43f23c156452e6a0e184d2ac8c9918c
malikobaid81 <122255795+malikobaid81@users.noreply.github.com>
2024-07-16 15:02:48 +0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "ap-south-1" 
+}
+
+variable "vpc_name" {
+       description = "The AWS vpc to create." 
+       default     = "terra-jenkins-vpc" 
+}
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "terra-jenkins-SG" 
+} 
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-0ad21ae1d0696ad58" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "terra-jenkins-ec2" 
+}
